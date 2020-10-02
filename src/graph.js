9d01a0c4f98b88fe0b186a590b13cda5f31f175d
Gabriel Davi <gdavi.gd@gmail.com>
2020-10-01 21:34:02 -0300

    Trying to apply Dijkstra algorithm

--- src/graph.js
+++ src/graph.js
@@ -1,65 +1,58 @@
 class Graph {
-	constructor() {
-		this.vertices = 0;
-        this.adjList = new Map();
-	}
-	
-	addVertex(v, info = {}) {
-		this.adjList.set(v, {...info, edges: []});
-	}
-	
-	addEdge(v1, v2, weight) {
-		this.adjList.get(v1).edges.push({v2, weight});
-		this.adjList.get(v2).edges.push({v1, weight});
-	}
-	
-	getVertex(v) {
-		return this.adjList.get(v);
-	}
+  constructor() {
+    this.INF = Number.MAX_SAFE_INTEGER;
+    this.vertices = 0;
+    this.adjList = new Map();
+  }
 
-	BFSShortestPath(vRoot, vDestination) {
-		if(vRoot == vDestination) {
-			return [vDestination]
-		}
-		var originTreeEdge = new Array();
-		var destinationTreeEdge = new Array();
-		var queue = new Array();
-		var visitedNodes = new Array();
-		var run = true;
-		
-		queue.push(vRoot);
-		visitedNodes.push(vRoot);
-		
-		while(queue.length > 0 && run) {
-			var uNode = queue.shift()
-			this.adjList.get(uNode).edges.forEach(vNode => {
-				if(!visitedNodes.includes(vNode) && run) {
-					originTreeEdge.unshift(uNode)
-					destinationTreeEdge.unshift(vNode)
-					if(vNode == vDestination || uNode == vDestination) {
-						run = false;
-					}
-					if(run) {
-						visitedNodes.push(vNode)
-						queue.push(vNode)
-					}
-				}
-			});
-		}
+  addVertex(v, info = {}) {
+    this.adjList.set(v, { ...info, edges: [] });
+  }
 
-		run = true;
-		var path = new Array()
-		var destination = vDestination;
-		destinationTreeEdge.forEach((node, index) => {
-			if(node == destination && run) {
-				path.unshift(node)
-				destination = originTreeEdge[index];
-				if(destination == vRoot) {
-					run = false;
-				}
-			}
-			
-		})
-		return path[0];
-	}
+  addEdge(v1, v2, weight) {
+    this.adjList.get(v1).edges.push({ connectedVertex: v2, weight });
+    this.adjList.get(v2).edges.push({ connectedVertex: v1, weight });
+  }
+
+  getVertex(v) {
+    return this.adjList.get(v);
+  }
+
+  dijkstra(src) {
+    const dist = [];
+    const visited = [];
+    const length = this.adjList.size;
+    for (let i = 0; i < length; i++) {
+      dist[i] = this.INF;
+      visited[i] = false;
+    }
+    dist[src] = 0;
+    for (let i = 0; i < length - 1; i++) {
+      const u = this.minDistance(dist, visited);
+      visited[u] = true;
+      for (let v = 0; v < length; v++) {
+        if (
+          !visited[v] &&
+          this.adjList.get(u).edges[v].connectedVertex !== 0 &&
+          dist[u] !== this.INF &&
+          dist[u] + graph[u][v] < dist[v]
+        ) {
+          dist[v] = dist[u] + this.adjList.get(u).edges[v].weight;
+        }
+      }
+    }
+    return dist;
+  }
+
+  minDistance(dist, visited) {
+    let min = this.INF;
+    let minIndex = -1;
+    for (let v = 0; v < dist.length; v++) {
+      if (visited[v] === false && dist[v] <= min) {
+        min = dist[v];
+        minIndex = v;
+      }
+    }
+    return minIndex;
+  }
 }
--- src/stage.js
+++ src/stage.js
@@ -1,69 +1,97 @@
 class Stage {
-	constructor(game, stage_name, tileset='tilemap') {
-		this.map = game.make.tilemap({ key: stage_name });
-		this.game = game
-		this.tileset = this.map.addTilesetImage(tileset);
+  constructor(game, stage_name, tileset = "tilemap") {
+    this.map = game.make.tilemap({ key: stage_name });
+    this.game = game;
+    this.tileset = this.map.addTilesetImage(tileset);
 
-		this.background_layer = this.map.createStaticLayer('Background', tileset, 0, 0);
-		this.wall_layer = this.map.createStaticLayer('Walls', tileset, 0, 0);
-		this.floor_layer = this.map.createStaticLayer('Floor', tileset, 0, 0);
-		this.poo_layer = this.map.createStaticLayer('Poo', tileset, 0, 0);
+    this.background_layer = this.map.createStaticLayer(
+      "Background",
+      tileset,
+      0,
+      0
+    );
+    this.wall_layer = this.map.createStaticLayer("Walls", tileset, 0, 0);
+    this.floor_layer = this.map.createStaticLayer("Floor", tileset, 0, 0);
+    this.poo_layer = this.map.createStaticLayer("Poo", tileset, 0, 0);
 
-		let stage_json = game.cache.json.get(stage_name + '_info');
-		this.spawn_point = stage_json.spawn_point;
-		this.enemies = stage_json.enemies;
-		this.end_area = stage_json.end_area;
-		this.items = stage_json.items;
-		
-		this.generateFloorGraph();
-		this.wall_layer.setCollisionBetween(0, 40000);
-		this.poo_layer.setCollisionBetween(0, 40000	);
-	}
-	
-	generateFloorGraph() {
-		this.floor_graph = new Graph();
-		let offset = this.floor_layer.layer.baseTileHeight/2;
-		
-		for(var x = 0; x < this.floor_layer.layer.height; x += 1)
-			for(var y = 0; y < this.floor_layer.layer.width; y += 1)
-				if(this.floor_layer.layer.data[x][y].index != -1)
-					this.floor_graph.addVertex(x * this.floor_layer.layer.width + y, {centerPosition: {y: x * this.floor_layer.layer.baseTileHeight + offset, x: y * this.floor_layer.layer.baseTileHeight + offset}, layer: 'Floor'});
-		
-		for(var x = 0; x < this.poo_layer.layer.height; x += 1)
-			for(var y = 0; y < this.poo_layer.layer.width; y += 1)
-				if(this.poo_layer.layer.data[x][y].index != -1)
-					this.floor_graph.addVertex(x * this.poo_layer.layer.width + y, {centerPosition: {y: x * this.poo_layer.layer.baseTileHeight + offset, x: y * this.poo_layer.layer.baseTileHeight + offset}, layer: 'Poo'});
+    let stage_json = game.cache.json.get(stage_name + "_info");
+    this.spawn_point = stage_json.spawn_point;
+    this.enemies = stage_json.enemies;
+    this.end_area = stage_json.end_area;
+    this.items = stage_json.items;
 
-					
-		this.floor_graph.adjList.forEach((vertex, id) => {
-			var rightVertex = this.floor_graph.getVertex(id + 1)
-			var underVertex = this.floor_graph.getVertex(id + this.floor_layer.layer.width)
-			var underRightVertex = this.floor_graph.getVertex(id + this.floor_layer.layer.width + 1)
+    this.generateFloorGraph();
+    this.wall_layer.setCollisionBetween(0, 40000);
+    this.poo_layer.setCollisionBetween(0, 40000);
+  }
 
-			if(rightVertex) {
-				if(rightVertex.layer == 'Floor') {
-					this.floor_graph.addEdge(id, id + 1, 1);
-				} else {
-					this.floor_graph.addEdge(id, id + 1, 2);
-				}
-			}
-				
-			if(underVertex){
-				if(underVertex.layer == 'Floor') {
-					this.floor_graph.addEdge(id, id + this.floor_layer.layer.width, 1);
-				} else {
-					this.floor_graph.addEdge(id, id + this.floor_layer.layer.width, 2);
-				}
-			}
-				
-			if(underRightVertex) {
-				if(underRightVertex.layer == 'Floor') {
-					this.floor_graph.addEdge(id, id + this.floor_layer.layer.width + 1, 1);
-				} else {
-					this.floor_graph.addEdge(id, id + this.floor_layer.layer.width + 1, 2);
-				}
-				
-			}
-		});
-	}
+  generateFloorGraph() {
+    this.floor_graph = new Graph();
+    let offset = this.floor_layer.layer.baseTileHeight / 2;
+
+    for (var x = 0; x < this.floor_layer.layer.height; x += 1)
+      for (var y = 0; y < this.floor_layer.layer.width; y += 1)
+        if (this.floor_layer.layer.data[x][y].index != -1)
+          this.floor_graph.addVertex(x * this.floor_layer.layer.width + y, {
+            centerPosition: {
+              y: x * this.floor_layer.layer.baseTileHeight + offset,
+              x: y * this.floor_layer.layer.baseTileHeight + offset,
+            },
+            layer: "Floor",
+          });
+
+    for (var x = 0; x < this.poo_layer.layer.height; x += 1)
+      for (var y = 0; y < this.poo_layer.layer.width; y += 1)
+        if (this.poo_layer.layer.data[x][y].index != -1)
+          this.floor_graph.addVertex(x * this.poo_layer.layer.width + y, {
+            centerPosition: {
+              y: x * this.poo_layer.layer.baseTileHeight + offset,
+              x: y * this.poo_layer.layer.baseTileHeight + offset,
+            },
+            layer: "Poo",
+          });
+
+    this.floor_graph.adjList.forEach((vertex, id) => {
+      var rightVertex = this.floor_graph.getVertex(id + 1);
+      var underVertex = this.floor_graph.getVertex(
+        id + this.floor_layer.layer.width
+      );
+      var underRightVertex = this.floor_graph.getVertex(
+        id + this.floor_layer.layer.width + 1
+      );
+
+      if (rightVertex) {
+        if (rightVertex.layer == "Floor") {
+          this.floor_graph.addEdge(id, id + 1, 1);
+        } else {
+          this.floor_graph.addEdge(id, id + 1, 2);
+        }
+      }
+
+      if (underVertex) {
+        if (underVertex.layer == "Floor") {
+          this.floor_graph.addEdge(id, id + this.floor_layer.layer.width, 1);
+        } else {
+          this.floor_graph.addEdge(id, id + this.floor_layer.layer.width, 2);
+        }
+      }
+
+      if (underRightVertex) {
+        if (underRightVertex.layer == "Floor") {
+          this.floor_graph.addEdge(
+            id,
+            id + this.floor_layer.layer.width + 1,
+            1
+          );
+        } else {
+          this.floor_graph.addEdge(
+            id,
+            id + this.floor_layer.layer.width + 1,
+            2
+          );
+        }
+      }
+    });
+    console.log(this.floor_graph.adjList);
+  }
 }
